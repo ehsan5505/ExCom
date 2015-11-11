235f649e3f1a1922f3b9aa715695684aacdb2796
Ehsan Rafeeque <dreamer_2142@yahoo.com>
2015-11-11 15:53:06 +0000

    link index to main.php to design main page of excom

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>ExCom || Welcome to the World of Products</title>
+    </head>
+    <body>
+        
+        <?php include("layout/main.php"); ?> 
+        
+    </body>
+    
+</html>
---
+++ layout/main.php
@@ -0,0 +1,10 @@
+<?php
+
+    // $title = "WORKING";
+
+    echo "<title>This is working fine</title>";
+    
+    echo "<script>alert('This is working');</script>";
+
+
+?>

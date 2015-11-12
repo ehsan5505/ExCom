a7141275d35c61c42227ece731103962002e60c1
Ehsan Rafeeque <dreamer_2142@yahoo.com>
2015-11-12 16:20:28 +0000

    adding the content on the pagination page

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <html>
     <head>
         <title>ExCom || Welcome to the World of Products</title>
+        <link href="style/materialize.css" rel="stylesheet" />
+        <link href="style/style.css" rel="stylesheet" />
     </head>
     <body>
         
-        <?php include("layout/main.php"); ?> 
+        <?php require_once("layout/pagination.php"); ?> 
         
+
+
+        <script type="text/javascript" src="scripts/jquery.js"></script>
+        <!--<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>-->
+        <script src="scripts/materialize.min.js"></script>
+        <script src="scripts/script.js" ></script>
     </body>
     
 </html>
